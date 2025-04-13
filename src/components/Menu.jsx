347b53171eea84d5a8f35ea2badbee88475a8765
Gangadhar184 <zenistuig65@gmail.com>
2025-04-13 17:54:13 +0530

    ui fix

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,31 +3,33 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import Shimmer from './Shimmer';
 import { MdStarRate } from 'react-icons/md';
-import RestaurantHeader from './RestaurantHeader';  
+import RestaurantHeader from './RestaurantHeader';
 import MenuCategory from './MenuCategory';
 
 const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300/";
 
 const Menu = () => {
-    
-    const {resId} = useParams();
+
+    const { resId } = useParams();
     const [menuList, setMenuList] = useState(null);
+    
 
-    const fetchMenuList = async () => {
-        try{
-            const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.493532&lng=78.440384&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`);
-            if(!data.ok){
-                throw new Error("Failed to fetch menu data");
+
+    useEffect(() => {
+        const fetchMenuList = async () => {
+            try {
+                const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.493532&lng=78.440384&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`);
+                if (!data.ok) {
+                    throw new Error("Failed to fetch menu data");
+                }
+                const json = await data.json();
+                console.log(json);
+                setMenuList(json.data);
+
+            } catch (error) {
+                console.error("Error fetching menu: ", error);
             }
-            const json = await data.json();
-            console.log(json);
-            setMenuList(json.data);
-
-        }catch(error){
-            console.error("Error fetching menu: ", error);
         }
-    }
-    useEffect(()=>{
         fetchMenuList();
     }, [resId]);
 
@@ -38,44 +40,44 @@ const Menu = () => {
     //     cuisines,
     //     locality,
     //     sla,} = menuList?.cards?.[2]?.card?.card?.info || {};
-        const restaurantInfo = menuList?.cards?.[2]?.card?.card?.info || {};
+    const restaurantInfo = menuList?.cards?.[2]?.card?.card?.info || {};
 
-        const cards = menuList?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards || [];
+    const cards = menuList?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards || [];
 
-         const categories = cards.map((card)=>card.card.card).filter((card)=>card["@type"]?.includes("ItemCategory"));
+    const categories = cards.map((card) => card.card.card).filter((card) => card["@type"]?.includes("ItemCategory"));
 
-        // const categories = cards.flatMap((card) => {
-        //     const cardData = card.card.card;
-        //     const type = cardData["@type"];
-          
-        //     if (type?.includes("ItemCategory")) {
-        //       return [cardData];
-        //     }
-          
-        //     if (type?.includes("NestedItemCategory") && Array.isArray(cardData.categories)) {
-        //       return cardData.categories;
-        //     }
-          
-        //     return [];
-        //   });
+    // const categories = cards.flatMap((card) => {
+    //     const cardData = card.card.card;
+    //     const type = cardData["@type"];
 
-    
-    if(!menuList) return <Shimmer/>
+    //     if (type?.includes("ItemCategory")) {
+    //       return [cardData];
+    //     }
 
-     return (
+    //     if (type?.includes("NestedItemCategory") && Array.isArray(cardData.categories)) {
+    //       return cardData.categories;
+    //     }
+
+    //     return [];
+    //   });
+
+
+    if (!menuList) return <Shimmer />
+
+    return (
         <div className='max-x-4xl mx-auto px-4 py-8'>
             <RestaurantHeader info={restaurantInfo} />
             {
-                categories.length?(
-                    categories.map((category)=>(
-                        <MenuCategory key={category.title} category={category}/>
+                categories.length ? (
+                    categories.map((category) => (
+                        <MenuCategory key={category.title} category={category} />
                     ))
-                ): (
+                ) : (
                     <h1>No Items available</h1>
                 )
             }
         </div>
-  );
+    );
 }
 
 export default Menu
